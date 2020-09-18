f5015825f93d59cb9f3b6c005d4ad3120d76f349
qjq2009cs <58396360@qq.com>
2020-09-18 23:39:56 +0800

    Update qjqibm.sh

--- qjqibm.sh
+++ qjqibm.sh
@@ -17,6 +17,10 @@ echo '<body>'>>index.php
 echo '</html>'>>index.php
 wget https://www.armn1.ml/entrypoint.sh
 chmod +x entrypoint.sh
+
+uuid=`cat /proc/sys/kernel/random/uuid`
+path=$(head /dev/urandom | tr -dc A-Za-z0-9 | head -c 16)
+
 echo 'applications:'>>manifest.yml
 echo '- path: .'>>manifest.yml
 echo '  command: '/app/htdocs/entrypoint.sh'' >>manifest.yml
@@ -28,19 +32,33 @@ ibmcloud cf push
 ibmyuming=$(ibmcloud app show $appname | grep h |awk '{print $2}'| awk -F: 'NR==2{print}')
     VMESSCODE=$(base64 -w 0 << EOF
     {
-      "v": "2",
-      "ps": "v2ws IBM",
-      "add": "$ibmyuming",
-      "port": "443",
-      "id": "8e82a9a5-b894-4b4d-bf2e-dbf0807572aa",
-      "aid": "4",
-      "net": "ws",
-      "type": "none",
-      "host": "",
-      "path": "/ws",
-      "tls": "tls"
+        "inbounds": [
+            {
+                "port": 8080,
+                "protocol": "vmess",
+                "settings": {
+                    "clients": [
+                        {
+                            "id": "${UUID}",
+                            "alterId": 4
+                        }
+                    ]
+                },
+                "streamSettings": {
+                    "network":"ws",
+                    "wsSettings": {
+                        "path": "${WSPATH}"
+                    }
+                }
+            }
+        ],
+        "outbounds": [
+            {
+                "protocol": "freedom",
+                "settings": {}
+            }
+        ]
     }
 EOF
-    )
-	echo "配置链接："
+    echo "配置链接："
     echo vmess://${VMESSCODE}
